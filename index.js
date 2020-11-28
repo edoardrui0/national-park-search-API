59700903ff2c816fb5132dba835197a683c366ca
Edoardo Ruiz <edoardrui0@gmail.com>
2020-11-28 16:17:59 -0500

    updated national park api

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ var requestOptions = {
 };
 
 let apiKey = "tZ6NWjkWvoNpgJc6WXhYEPNwl59yJyXbw1U5FNRD";
-let url = `https://developer.nps.gov/api/v1/parks?api_key=${apiKey}`;
 
-function getParks() {
-  fetch(url)
+function getParks(stateCode, limit) {
+  fetch(
+    `https://developer.nps.gov/api/v1/parks?api_key=${apiKey}&stateCode=${stateCode}&limit=${limit}`
+  )
     .then((response) => {
       if (response.ok) {
         return response.json();
@@ -30,48 +31,32 @@ function getParks() {
 }
 
 function displayParks(responseJson) {
-  // if there are previous results, remove them
   $(".results").empty();
-  // iterate through the articles array, stopping at the max number of results
+
   for (let i = 0; i < responseJson.data.length; i++) {
-    let nameOfParks = $('input[name="nationalParkName"]').val().toUpperCase();
-    if (nameOfParks === responseJson.data[i].states) {
-      $(".results").append(
-        `<div class="parkInformation">
-          <h4>${responseJson.data[i].fullName}</h4>
-          <p>${responseJson.data[i].description}</p>
-          <p><a href="${responseJson.data[i].url}" target="_blank">Click here to visit the park's website</a></p>
-         </div>`
-      );
-    }
+    $(".results").append(`
+      <div class="parkInformation">
+      <h4>${responseJson.data[i].fullName}</h4>
+      <p>${responseJson.data[i].description}</p>
+      <p><a href="${responseJson.data[i].url}" target="_blank">Click here to visit the park's website</a></p>
+     </div>`);
   }
-  //display the results section
+
   $(".results").removeClass("hidden");
 }
-
-// function multipleParkNames() {
-//   let nameOfParks = $('input[name="nationalParkName"]').val();
-// }
 
 function watchForm() {
   $("form").submit((event) => {
     event.preventDefault();
-    let nameOfParks = $('input[name="nationalParkName"]').val().toUpperCase();
-    getParks(nameOfParks);
+    let stateCode = $('input[name="nationalParkName"]').val().toUpperCase();
+    let limit = $('input[name="max-results"]').val();
+    getParks(stateCode, limit);
+    console.log(stateCode, limit);
   });
 }
 
 $(function () {
   console.log("App loaded! Waiting for submit!");
   watchForm();
+  // getParks("NY,FL", 15);
 });
-
-// Requirements:
-// 1) The user must be able to search for parks in one or more states.
-// 2) The user must be able to set the max number of results, with a default of 10.
-// 3) The search must trigger a call to NPS's API.
-// 4) The parks in the given state must be displayed on the page. Include at least:
-//    * Full name
-//    * Description
-//    * Website URL
-// 5) The user must be able to make multiple searches and see only the results for the current search.
